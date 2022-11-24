e23a7fa8c011b7cb776324e68ece64c7a5115bdf
Awes <28003@ma-web.nl>
2022-11-24 10:21:30 +0100

    Variables gemaakt!

---
+++ les1/app.js
@@ -0,0 +1,24 @@
+class App
+{
+    runApplication()
+    {
+        console.log("Hello World!")
+        let appNaam = "Cool";
+        let versienummer = 0.9;
+        const datum =  "2022-11-24";
+        let autheur = "Awes Zoretic";
+        let copyright = "MediaCollege";
+        let distributeur = "Nederland";
+        let darkmode = true;
+        console.log(appNaam);
+        console.log(versienummer);
+        console.log(datum);
+        console.log(autheur);
+        console.log (distributeur);
+        console.log(darkmode);
+    }
+
+}
+let app = new App();
+app.runApplication();
+
