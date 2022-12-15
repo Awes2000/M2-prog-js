bd46737dc4ddc030cb0badc421067e0cdf12bf9e
Awes <28003@ma-web.nl>
2022-12-15 10:12:38 +0100

    classes test

---
+++ classstappen.js
@@ -0,0 +1,13 @@
+class Auto
+{
+    runAuto()
+    {
+    this.onderdelen
+    let onderdelen = ["uitlaat", "wielen", "motor", "gaspedaal"]
+    console.log(onderdelen)
+    }
+
+}
+
+let Auto = new Auto();
+Auto.runAuto();
